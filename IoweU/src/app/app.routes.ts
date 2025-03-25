f706032543314598cdea704d544becf9b32b1a2c
Jakob-ui <Jakoblaschober@gmail.com>
2025-03-25 11:54:07 +0100

    Add registration page and routing; update login page with register link

--- IoweU/src/app/app.routes.ts
+++ IoweU/src/app/app.routes.ts
@@ -14,5 +14,9 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+  },
 
 ];
