d63ec67fcf4e0f521b3227f787e00cc626c91002
Livia130503 <mt221054@fhstp.ac.at>
2025-03-25 10:38:48 +0100

    Add login page and routing; update home page with login button

--- IoweU/src/app/home/home.page.ts
+++ IoweU/src/app/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFooter } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonButtons, IonButton } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonFooter, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonFooter, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, RouterModule],
+  standalone: true
 })
 export class HomePage {
   constructor() {}
