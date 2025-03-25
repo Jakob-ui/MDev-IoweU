48384ad1bc83d8a3d052cca95f2c53095ea0f6df
Jakob-ui <Jakoblaschober@gmail.com>
2025-03-25 10:12:03 +0100

    Update Firebase configuration for IoweU project

--- IoweU/src/environments/environment.prod.ts
+++ IoweU/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAqHM8qaC73wcI5LfO9bcQKGS438WnavTA",
+    authDomain: "mdev-ioweu.firebaseapp.com",
+    projectId: "mdev-ioweu",
+    storageBucket: "mdev-ioweu.firebasestorage.app",
+    messagingSenderId: "372900673752",
+    appId: "1:372900673752:web:4f72cc1a005f043ddfbfad",
+    measurementId: "G-QVRYBBR6P6"
+  }
 };
