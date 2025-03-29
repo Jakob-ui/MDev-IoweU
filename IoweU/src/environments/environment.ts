5ff1a3edd05bf8da7b28f6b54fb9fcecea8539ec
Jakob-ui <Jakoblaschober@gmail.com>
2025-03-29 15:54:21 +0100

    aupdated api-key

--- IoweU/src/environments/environment.ts
+++ IoweU/src/environments/environment.ts
@@ -1,24 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAqHM8qaC73wcI5LfO9bcQKGS438WnavTA",
-    authDomain: "mdev-ioweu.firebaseapp.com",
-    projectId: "mdev-ioweu",
-    storageBucket: "mdev-ioweu.firebasestorage.app",
-    messagingSenderId: "372900673752",
-    appId: "1:372900673752:web:4f72cc1a005f043ddfbfad",
-    measurementId: "G-QVRYBBR6P6"
-  }
+    apiKey: 'AIzaSyDn4hAduYYpx_ERORW58vl-I1IjewZey9o',
+    authDomain: 'ioweu-a74cd.firebaseapp.com',
+    projectId: 'ioweu-a74cd',
+    storageBucket: 'ioweu-a74cd.firebasestorage.app',
+    messagingSenderId: '380190786410',
+    appId: '1:380190786410:web:ebbab06b1bf5214da6db13',
+  },
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
