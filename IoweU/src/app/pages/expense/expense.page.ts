9e5ad9eedeed154e628231357c3596ee90192a9f
Livia130503 <mt221054@fhstp.ac.at>
2025-05-08 18:33:35 +0200

    Enhance expense categories and update card background color for improved UI

--- IoweU/src/app/pages/expense/expense.page.ts
+++ IoweU/src/app/pages/expense/expense.page.ts
@@ -95,6 +95,17 @@ export class ExpensePage implements OnInit, OnDestroy {
     { name: 'Freizeit', icon: 'game-controller-outline' },
     { name: 'Wohnen', icon: 'home-outline' },
     { name: 'Rechnungen', icon: 'receipt-outline' },
+    { name: 'Gesundheit', icon: 'heart-outline' },
+    { name: 'Bildung', icon: 'school-outline' },
+    { name: 'Arbeit', icon: 'briefcase-outline' },
+    { name: 'Kleidung', icon: 'shirt-outline' },
+    { name: 'Finanzen', icon: 'cash-outline' },
+    { name: 'Reisen', icon: 'airplane-outline' },
+    { name: 'Geschenke', icon: 'gift-outline' },
+    { name: 'Haushalt', icon: 'hammer-outline' },
+    { name: 'Ideen/Projekte', icon: 'bulb-outline' },
+    { name: 'Musik/Events', icon: 'musical-notes-outline' },
+    { name: 'Technik', icon: 'rocket-outline' },
     { name: 'Sonstiges', icon: 'ellipsis-horizontal-outline' },
   ];
 
