7730a2d5be657f1a14c4255cc5d3ffb29b8ec341
Jakob Laschober <64865305+Jakob-ui@users.noreply.github.com>
2025-05-12 10:32:36 +0200

    added new api keys

--- IoweU/src/environments/environment.ts
+++ IoweU/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyDn4hAduYYpx_ERORW58vl-I1IjewZey9o',
+    apiKey: 'AIzaSyAddQpF6q9jqv99WG0WT02AjRhK7wFnrSg',
     authDomain: 'ioweu-a74cd.firebaseapp.com',
     projectId: 'ioweu-a74cd',
     storageBucket: 'ioweu-a74cd.firebasestorage.app',
