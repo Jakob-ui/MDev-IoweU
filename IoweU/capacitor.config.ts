9f7a537721ff49825ea1fa6ca0c3375d975128f0
Michaela Kopf <mt221080@fhstp.ac.at>
2025-05-23 09:19:47 +0200

    Füge Unterstützung für Push-Benachrichtigungen hinzu und aktualisiere die Konfiguration

--- IoweU/capacitor.config.ts
+++ IoweU/capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'ioweu.eu',
   appName: 'IoweU',
   webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  },
 };
 
 export default config;
